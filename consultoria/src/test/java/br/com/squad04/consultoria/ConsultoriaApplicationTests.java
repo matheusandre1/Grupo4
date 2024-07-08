d542f11bb689306d01d63b9732b270c5cae9a9fd
washiner takeuchi <washiner@MacBook-Air-de-washiner.local>
2024-07-08 12:22:06 -0400

    teste git

---
+++ consultoria/src/test/java/br/com/squad04/consultoria/ConsultoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.squad04.consultoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
