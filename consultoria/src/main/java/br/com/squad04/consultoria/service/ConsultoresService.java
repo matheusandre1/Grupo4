a0b6c0350beb894021ec94505edefc33eaeef883
Fabricio Fragoso <49501140+fabriciofragoso00@users.noreply.github.com>
2024-07-09 21:16:45 -0300

    Add files via upload

---
+++ consultoria/src/main/java/br/com/squad04/consultoria/service/ConsultoresService.java
@@ -0,0 +1,41 @@
+package br.com.squad04.consultoria.service;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.squad04.consultoria.model.Consultores;
+import br.com.squad04.consultoria.repository.ConsultoresRepository;
+
+@Service
+public class ConsultoresService {
+    @Autowired
+    private ConsultoresRepository consultoresRepository;
+
+    public List<Consultores> getAllConsultores(){
+        return consultoresRepository.findAll();
+    }
+
+    public Optional<Consultores> getConsultorById(long idConsultor){
+        return consultoresRepository.findById(idConsultor);
+    }
+
+    public Consultores saveConsultor(Consultores consultor){
+        if (consultor.getIdConsultor() == null) {
+            // Novo consultor, definir data de cadastro
+            consultor.setDataCadastro(new Timestamp(System.currentTimeMillis()));
+        } else {
+            // Atualização, manter a data de cadastro original
+            Consultores existingConsultor = consultoresRepository.findById(consultor.getIdConsultor()).orElseThrow();
+            consultor.setDataCadastro(existingConsultor.getDataCadastro());
+        }
+        return consultoresRepository.save(consultor);
+    }
+
+    public void deleteConsultor(long idConsultor){
+        consultoresRepository.deleteById(idConsultor);
+    }
+}
