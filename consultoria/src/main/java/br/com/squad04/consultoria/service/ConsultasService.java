24299f616cc6b44983dbff574330a3bfd6e9210f
Fabricio Fragoso <49501140+fabriciofragoso00@users.noreply.github.com>
2024-07-18 09:37:52 -0300

    Update ConsultasService.java

--- consultoria/src/main/java/br/com/squad04/consultoria/service/ConsultasService.java
+++ consultoria/src/main/java/br/com/squad04/consultoria/service/ConsultasService.java
@@ -1,13 +1,32 @@
 package br.com.squad04.consultoria.service;
 
-import br.com.squad04.consultoria.model.Consultas;
-
 import java.util.List;
 import java.util.Optional;
 
-public interface ConsultasService {
-    Consultas saveConsulta(Consultas consulta);
-    List<Consultas> getAllConsultas();
-    Optional<Consultas> getConsultaById(Long id);
-    void deleteConsulta(Long id);
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.squad04.consultoria.model.Consultas;
+import br.com.squad04.consultoria.repository.ConsultasRepository;
+
+@Service
+public class ConsultasService {
+    @Autowired
+    private ConsultasRepository consultasRepository;
+
+    public List<Consultas> getAllConsultas(){
+        return consultasRepository.findAll();
+    }
+
+    public Optional<Consultas> getConsultaById(long idConsulta){
+        return consultasRepository.findById(idConsulta);
+    }
+
+    public Consultas saveConsulta(Consultas consulta){
+        return consultasRepository.save(consulta);
+    }
+
+    public void deleteConsulta(long idConsulta){
+        consultasRepository.deleteById(idConsulta);
+    }
 }
