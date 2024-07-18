f3f090b68397976bb3ef715a178ec789d8d23e3f
Fabricio Fragoso <fabriciofragoso55@gmail.com>
2024-07-18 19:12:59 -0300

    add projeto atualizado

---
+++ projeto__v2/consultoria/src/main/java/br/com/squad04/consultoria/ConsultoriaApplication.java
@@ -0,0 +1,14 @@
+package br.com.squad04.consultoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultoriaApplication.class, args);
+		System.out.println("teste");
+	}
+
+}
