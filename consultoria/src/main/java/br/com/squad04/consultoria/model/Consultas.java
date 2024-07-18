16ec749ec312fe22be80877993c6230fd00a340f
Fabricio Fragoso <49501140+fabriciofragoso00@users.noreply.github.com>
2024-07-18 09:36:06 -0300

    Update Consultas.java

--- consultoria/src/main/java/br/com/squad04/consultoria/model/Consultas.java
+++ consultoria/src/main/java/br/com/squad04/consultoria/model/Consultas.java
@@ -1,67 +1,58 @@
 package br.com.squad04.consultoria.model;
 
-import jakarta.persistence.*;
-import java.time.LocalDateTime;
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Consultas {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idConsulta;
-
+    private Date dataConsulta;
+    private String horaConsulta;
+    
     @ManyToOne
     @JoinColumn(name = "id_cliente", nullable = false)
-    private Clientes cliente;
-
+    private Clientes idCliente;
+    
     @ManyToOne
     @JoinColumn(name = "id_consultor", nullable = false)
-    private Consultores consultor;
-
-    private LocalDateTime dataHora;
-    private String status;
-
+    private Consultores idConsultor;
+    
     public Long getIdConsulta() {
         return idConsulta;
     }
-
     public void setIdConsulta(Long idConsulta) {
         this.idConsulta = idConsulta;
     }
-
+    public Date getDataConsulta() {
+        return dataConsulta;
+    }
+    public void setDataConsulta(Date dataConsulta) {
+        this.dataConsulta = dataConsulta;
+    }
+    public String getHoraConsulta() {
+        return horaConsulta;
+    }
+    public void setHoraConsulta(String horaConsulta) {
+        this.horaConsulta = horaConsulta;
+    }
     public Clientes getCliente() {
-        return cliente;
+        return idCliente;
     }
-
-    public void setCliente(Clientes cliente) {
-        this.cliente = cliente;
+    public void setCliente(Clientes idCliente) {
+        this.idCliente = idCliente;
     }
-
     public Consultores getConsultor() {
-        return consultor;
+        return idConsultor;
     }
-
-    public void setConsultor(Consultores consultor) {
-        this.consultor = consultor;
-    }
-
-    public LocalDateTime getDataHora() {
-        return dataHora;
-    }
-
-    public void setDataHora(LocalDateTime dataHora) {
-        this.dataHora = dataHora;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void definirDataHoraAtual() {
-        this.dataHora = LocalDateTime.now();
+    public void setConsultor(Consultores idConsultor) {
+        this.idConsultor = idConsultor;
     }
 }
