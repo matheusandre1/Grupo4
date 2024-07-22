4b1ffb41f7c737d6fe8095713cc20350e06c63f7
Lucianapoppi <80785603+Lucianapoppi@users.noreply.github.com>
2024-07-22 20:43:12 -0300

    Luciana: Alteracao na visualizacao do cliente e do consultor e criacao do administrador

---
+++ consultoria/src/main/resources/static/js/verificadorDeTipoDeUsuario.js
@@ -0,0 +1,29 @@
+﻿
+
+document.addEventListener('DOMContentLoaded', function () {
+    const tipoDeUsuario = localStorage.getItem('tipoDeUsuario');
+    const botoesDeletar = document.querySelectorAll(".deletar-btn");
+    const botoesEditar = document.querySelectorAll(".editar-btn");
+    const novo = document.querySelector(".novo-btn");
+    console.log("entrando na função")
+    if (tipoDeUsuario === 'cliente') {
+
+        botoesDeletar.forEach(botao => {
+            botao.style.display = 'none';
+        });
+
+        botoesEditar.forEach(botao => {
+            botao.style.display = 'none';
+        });
+
+        if (novo) {
+            novo.style.display = 'none';
+        }
+        const intervalId = setInterval(checkAndHideElements, 250); // Verifica a cada 500ms
+
+    }else if(tipoDeUsuario === 'consultor'){
+        botoesDeletar.forEach(botao => {
+            botao.style.display = 'none';
+        });
+    }
+});
